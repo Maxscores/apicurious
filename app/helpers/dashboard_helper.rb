75bdecce90be3310cc6e289fbb5f0f65a375ea73
maxscores <maxstackhouse@gmail.com>
2018-01-30 13:20:22 -0700

    Refactor out Repositories

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,15 @@
 module DashboardHelper
+  def tab_selection
+    if @tab == "repositories"
+      render "dashboard/repositories"
+    elsif @tab == "stars"
+      render "dashboard/stars"
+    elsif @tab == "followers"
+      render "dashboard/followers"
+    elsif @tab == "following"
+      render "dashboard/following"
+    else
+      render "dashboard/overview"
+    end
+  end
 end
