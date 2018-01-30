e74a4735b19ebc2b8636c258312b6e036eda9c32
maxscores <maxstackhouse@gmail.com>
2018-01-30 13:19:51 -0700

    Minor CSS mod

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,6 @@
 class DashboardController < ApplicationController
   def index
     @presenter = DashboardPresenter.new(current_user)
-    # memoize inside presenter
-    # make methods live in presenter
     @tab = params[:tab]
   end
 end
