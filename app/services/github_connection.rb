74b3a28029fee2bbefb9892882d102dc5f9ffe7a
maxscores <maxstackhouse@gmail.com>
2018-01-31 11:36:14 -0700

    Move Github API to Services

--- app/services/github_connection.rb
+++ app/services/github_connection.rb
@@ -7,46 +7,41 @@ class GithubConnection
   end
 
   def github_connection
-    Faraday.new(:url => "https://api.github.com", :headers => {"Authorization" => "token #{token}"})
-  end
-
-  def get_url(url)
-    github_connection.get do |req|
-      req.url url
-    end
+    Faraday.new(:url => "https://api.github.com",
+                :headers => {"Authorization" => "token #{token}"})
   end
 
   def get_user_info
-    response = get_url('/user')
+    response = github_connection.get('/user')
     GithubUser.new(JSON.parse(response.body))
   end
 
   def get_repos
-    response = get_url("/users/#{username}/repos")
+    response = github_connection.get("/users/#{username}/repos")
     JSON.parse(response.body).map do |repo|
       GithubRepo.new(repo)
     end
   end
 
   def get_stars
-    response = get_url("/users/#{username}/starred")
+    response = github_connection.get("/users/#{username}/starred")
     JSON.parse(response.body).map do |star|
       GithubRepo.new(star)
     end
   end
 
   def get_followers
-    response = get_url("/user/followers")
+    response = github_connection.get("/user/followers")
     JSON.parse(response.body).map do |user|
-      user = JSON.parse(get_url("/users/#{user["login"]}").body)
+      user = JSON.parse(github_connection.get("/users/#{user["login"]}").body)
       GithubUser.new(user)
     end
   end
 
   def get_following
-    response = get_url("/user/following")
+    response = github_connection.get("/user/following")
     JSON.parse(response.body).map do |user|
-      user = JSON.parse(get_url("/users/#{user["login"]}").body)
+      user = JSON.parse(github_connection.get("/users/#{user["login"]}").body)
       GithubUser.new(user)
     end
   end
