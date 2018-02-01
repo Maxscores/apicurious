2e6afbb7895011bd640759c0204f5bd879a26085
maxscores <maxstackhouse@gmail.com>
2018-02-01 11:04:31 -0700

    Add github_repo spec

--- app/services/github_repo.rb
+++ app/services/github_repo.rb
@@ -10,16 +10,16 @@ class GithubRepo
               :language,
               :updated_at
 
-  def initialize(repo)
-    @name = repo[:name]
-    @owner = repo[:owner]
-    @full_name = repo[:full_name]
-    @description = repo[:description]
-    @private = repo[:private]
-    @fork = repo[:fork]
-    @api_url = repo[:url]
-    @http_url = repo[:http_url]
-    @language = repo[:language]
-    @updated_at = repo[:updated_at]
+  def initialize(repo_attr)
+    @name = repo_attr[:name]
+    @owner = repo_attr[:owner]
+    @full_name = repo_attr[:full_name]
+    @description = repo_attr[:description]
+    @private = repo_attr[:private]
+    @fork = repo_attr[:fork]
+    @api_url = repo_attr[:url]
+    @http_url = repo_attr[:http_url]
+    @language = repo_attr[:language]
+    @updated_at = repo_attr[:updated_at]
   end
 end
