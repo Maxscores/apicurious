e3ac3f55b3a6259b05d4c30e8b458fcada77b28c
maxscores <maxstackhouse@gmail.com>
2018-02-01 10:54:40 -0700

    Changed JSON names from string to symbols

--- app/services/github_repo.rb
+++ app/services/github_repo.rb
@@ -11,15 +11,15 @@ class GithubRepo
               :updated_at
 
   def initialize(repo)
-    @name = repo["name"]
-    @owner = repo["owner"]
-    @full_name = repo["full_name"]
-    @description = repo["description"]
-    @private = repo["private"]
-    @fork = repo["fork"]
-    @api_url = repo["url"]
-    @http_url = repo["http_url"]
-    @language = repo["language"]
-    @updated_at = repo["updated_at"]
+    @name = repo[:name]
+    @owner = repo[:owner]
+    @full_name = repo[:full_name]
+    @description = repo[:description]
+    @private = repo[:private]
+    @fork = repo[:fork]
+    @api_url = repo[:url]
+    @http_url = repo[:http_url]
+    @language = repo[:language]
+    @updated_at = repo[:updated_at]
   end
 end
--- app/services/github_user.rb
+++ app/services/github_user.rb
@@ -13,17 +13,17 @@ class GithubUser < GithubConnection
               :email
 
   def initialize(user_info)
-    @username = user_info["login"]
-    @avatar_url = user_info["avatar_url"]
-    @full_name = user_info["name"]
-    @bio = user_info["bio"]
-    @public_repos_count = user_info["public_repos"]
-    @public_gists_count = user_info["public_gists"]
-    @followers_count = user_info["followers"]
-    @following_count = user_info["following"]
-    @location = user_info["location"]
-    @company = user_info["company"]
-    @blog = user_info["blog"]
-    @email = user_info["email"]
+    @username = user_info[:login]
+    @avatar_url = user_info[:avatar_url]
+    @full_name = user_info[:name]
+    @bio = user_info[:bio]
+    @public_repos_count = user_info[:public_repos]
+    @public_gists_count = user_info[:public_gists]
+    @followers_count = user_info[:followers]
+    @following_count = user_info[:following]
+    @location = user_info[:location]
+    @company = user_info[:company]
+    @blog = user_info[:blog]
+    @email = user_info[:email]
   end
 end
